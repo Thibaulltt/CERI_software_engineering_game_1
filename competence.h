d4addc5ed79b1eba16d4a2b3e96b72adeff9caf3
Raph-already-taken <raflegrand@hotmail.com>
2017-04-29 18:50:44 +0200

    Add files via upload

---
+++ competence.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#ifndef COMPETENCE_H
+#define COMPETENCE_H
+#pragma once
+
+class competence
+{
+	std::string skillName;
+	int skillDamage;
+	int skillManaCost;
+
+public:
+	competence();
+	competence(std::string skillName, int skillDamage, int skillManaCost);
+	competence(std::string skillName, int skillDamage);
+	~competence();
+
+	std::string getName();
+	std::string getDescription();
+	int getDamage();
+	int getManaCost();
+
+	template<typename T> std::string toString(const T & valeur);
+
+	void afficher_detail(); //Test
+
+	std::string competenceString(); //Convertit les carac. d'une comp�tence en une string format�e
+};
+
+#endif
