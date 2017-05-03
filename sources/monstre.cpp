7666adaf8c1abe60820199964f6e922fc0e45dc3
Thibaulltt <devillelethibault@gmail.com>
2017-05-03 23:59:29 +0200

    pour eviter les conflits
    rajouté la fonction monstre::printMonstre

--- sources/monstre.cpp
+++ sources/monstre.cpp
@@ -11,3 +11,19 @@ monstre::monstre()
 	entite();
 	this -> entiteId = "m";
 }
+
+void monstre::printMonstre()
+{
+	cout << "Nom: " << this->entiteName<< endl;
+	cout << "HP: " << this->entiteHpCurrent << "/" << this->entiteHpMax;
+	cout << "Vitesse: " << this->entiteSpeed<< endl;
+
+	cout << endl << "Compétences: " << endl;
+
+	for (int i=0 ; i<this->entiteSkillVect.size() ; i++)
+	{
+		this->entiteSkillVect[i].afficher_detail();
+	}
+
+	cout << endl;
+}
